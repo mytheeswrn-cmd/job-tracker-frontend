062b3998e552c482de1cc523599797a5cca2cf7c
Kanimozhi <Kanimozhiramkumar03@gmail.com>
2026-06-18 10:01:58 +0530

    Fix http to https in api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://job-tracker-production-7597.up.railway.app/api',
+  baseURL: 'https://job-tracker-production-7597.up.railway.app/api',
 });
 
 // Automatically attach the JWT token to every request, if one exists
